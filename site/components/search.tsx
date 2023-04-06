b61cfe0e99e17c46beba0aa7d4081d0f11fa7d5d
Mustafa Hoda <mustafa@swell.is>
2023-04-06 15:56:58 -0500

    test commit

--- site/components/search.tsx
+++ site/components/search.tsx
@@ -234,7 +234,7 @@ export default function Search({ categories, brands }: SearchPropsType) {
                             'block lg:inline-block px-4 py-2 lg:p-0 lg:my-2 lg:mx-4'
                           }
                         >
-                          All Designers
+                          All Designersasd
                         </a>
                       </Link>
                     </li>
